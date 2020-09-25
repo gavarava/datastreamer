c649c5bf12ec4d18ad7d7586528d10cd87b1295f
Gaurav Edekar <edekar.gaurav@gmail.com>
2020-09-25 16:59:50 +0200

    Update Order Specification

--- src/main/java/com/postgres/bigdata/OrderSpecification.java
+++ src/main/java/com/postgres/bigdata/OrderSpecification.java
@@ -5,10 +5,25 @@ import org.springframework.data.jpa.domain.Specification;
 
 public abstract class OrderSpecification {
 
+  private static final String ORDERTOTAL_COLUMN = "ordertotal";
+
   public static Specification<OrderEntity> orderTotalLessThan(BigDecimal amount) {
     return (Specification<OrderEntity>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
-        .lessThanOrEqualTo(root.get("ordertotal"), amount);
+        .lessThan(root.get(ORDERTOTAL_COLUMN), amount);
+  }
+
+  public static Specification<OrderEntity> orderTotalLessThanOrEqualTo(BigDecimal amount) {
+    return (Specification<OrderEntity>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
+        .lessThanOrEqualTo(root.get(ORDERTOTAL_COLUMN), amount);
+  }
+
+  public static Specification<OrderEntity> orderTotalGreaterThan(BigDecimal amount) {
+    return (Specification<OrderEntity>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
+        .greaterThan(root.get(ORDERTOTAL_COLUMN), amount);
+  }
+
+  public static Specification<OrderEntity> orderTotalGreaterThanOrEqualTo(BigDecimal amount) {
+    return (Specification<OrderEntity>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
+        .greaterThanOrEqualTo(root.get(ORDERTOTAL_COLUMN), amount);
   }
 }
-
-
